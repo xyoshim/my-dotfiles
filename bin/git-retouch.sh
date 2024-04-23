0e1c2764dbaea521504957c6e3694c3ebcb94271
MVA <mva000@gmail.com>
2024-04-23 18:09:33 +0900

    Git用スクリプトの追加

---
+++ bin/git-retouch.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+for file in $(git ls-files); do
+  timestamp=$(git log -1 --date=format-local:"%Y%m%d%H%M%S" --pretty=format:"%ad" "${file}")
+  echo touch -t "${timestamp}" "${file}"
+done
