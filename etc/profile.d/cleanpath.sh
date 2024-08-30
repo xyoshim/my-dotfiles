ab7b520c7731cfb55a5603d0d78ef0a79c790dd7
MVA <mva000@gmail.com>
2024-08-30 17:42:36 +0900

    WindowsネイティブPerlのパスを削除

--- etc/profile.d/cleanpath.sh
+++ etc/profile.d/cleanpath.sh
@@ -3,14 +3,16 @@ TMP_PATH=$(echo :${PATH}: | sed \
   -e 's|:/cygdrive/c/[Ww][Ii][Nn][Ds][Oo][Ww][Ss]/System32/WindowsPowerShell/v1.0|:|g' \
   -e 's|:/cygdrive/c/[Ww][Ii][Nn][Ds][Oo][Ww][Ss]/System32/OpenSSH:|:|g' \
   -e 's|:/cygdrive/c/Program Files/PowerShell/7:|:|g' \
-  -e 's|:/cygdrive/c/Program Files/dotnet:|:|g' \
   -e 's|:/cygdrive/c/Program Files (x86)/dotnet:|:|g' \
   -e 's|:/cygdrive/c/Program Files (x86)/Windows Kits/10/Windows Performance Toolkit:|:|g' \
   -e 's|:/cygdrive/c/Program Files (x86)/NVIDIA Corporation/PhysX/Common:|:|g' \
+  -e 's|:/cygdrive/c/Strawberry/perl/bin:|:|g' \
+  -e 's|:/cygdrive/c/Strawberry/perl/site/bin:|:|g' \
+  -e 's|:/cygdrive/c/Strawberry/c/bin:|:|g' \
+  -e 's|:/cygdrive/c/Program Files/dotnet:|:|g' \
   -e 's|:/cygdrive/c/Program Files/LLVM/bin:|:|g' \
   -e 's|:/cygdrive/c/Program Files/Git/cmd:|:|g' \
   -e 's|:/cygdrive/c/Program Files/CMake/bin:|:|g' \
-  -e 's|:/cygdrive/c/Program Files/PowerShell/7:|:|g' \
   -e "s|:/cygdrive/c/Users/${USERNAME:-${USER}}/AppData/Local/Programs/Python/Python[0-9][0-9][0-9]/Scripts:|:|g" \
   -e "s|:/cygdrive/c/Users/${USERNAME:-${USER}}/AppData/Local/Programs/Python/Python[0-9][0-9][0-9]:|:|g" \
   -e "s|:/cygdrive/c/Users/${USERNAME:-${USER}}/AppData/Local/Microsoft/WindowsApps:|:|g" \
