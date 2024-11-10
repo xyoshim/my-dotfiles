d92535b1a4cb6b641a19cea88113dd7122d1d89d
MVA <mva000@gmail.com>
2024-11-10 20:50:40 +0900

    干渉するWindowsパスの削除方法を修正
     * 不要なコロン":"を削除
     * WIP: 同じ処理を2回繰り返さないと削除できないパスがある

--- etc/profile.d/cleanpath.sh
+++ etc/profile.d/cleanpath.sh
@@ -1,5 +1,5 @@
 #
-TMP_PATH=$(echo :${PATH}: | sed \
+TMP_PATH=$(echo :"${PATH}": | sed \
   -e 's|:/cygdrive/c/[Ww][Ii][Nn][Ds][Oo][Ww][Ss]/System32/WindowsPowerShell/v1.0|:|g' \
   -e 's|:/cygdrive/c/[Ww][Ii][Nn][Ds][Oo][Ww][Ss]/System32/OpenSSH:|:|g' \
   -e 's|:/cygdrive/c/Program Files/PowerShell/7:|:|g' \
@@ -13,14 +13,20 @@ TMP_PATH=$(echo :${PATH}: | sed \
   -e 's|:/cygdrive/c/Program Files/LLVM/bin:|:|g' \
   -e 's|:/cygdrive/c/Program Files/Git/cmd:|:|g' \
   -e 's|:/cygdrive/c/Program Files/CMake/bin:|:|g' \
-  -e "s|:/cygdrive/c/Users/${USERNAME:-${USER}}/AppData/Local/Programs/Python/Python[0-9][0-9][0-9]/Scripts:|:|g" \
-  -e "s|:/cygdrive/c/Users/${USERNAME:-${USER}}/AppData/Local/Programs/Python/Python[0-9][0-9][0-9]:|:|g" \
   -e "s|:/cygdrive/c/Users/${USERNAME:-${USER}}/AppData/Local/Microsoft/WindowsApps:|:|g" \
   -e "s|:/cygdrive/c/Users/${USERNAME:-${USER}}/AppData/Local/Android/Sdk/platform-tools:|:|g" \
   -e "s|:/cygdrive/c/Users/${USERNAME:-${USER}}/AppData/Local/Programs/Microsoft VS Code/bin:|:|g" \
   -e "s|:/cygdrive/c/Users/${USERNAME:-${USER}}/.dotnet/tools:|:|g" \
-  -e 's|:+|:|g' -e 's|^:||' -e 's|:$||' \
+)
+TMP_PATH=$(echo :"${TMP_PATH}": | sed \
+  -E "s|:/cygdrive/c/Users/${USERNAME:-${USER}}/AppData/Local/Programs/Python/Python[-.0-9a-zA-Z]+:|:|g" \
+)
+TMP_PATH=$(echo :"${TMP_PATH}": | sed \
+  -E "s|:/cygdrive/c/Users/${USERNAME:-${USER}}/AppData/Local/Programs/Python/Python[-/.0-9a-zA-Z]+:|:|g" \
+)
+TMP_PATH=$(echo :"${TMP_PATH}": | sed \
+  -E "s|:/cygdrive/c/Users/${USERNAME:-${USER}}/AppData/Local/Programs/Python/Python[-/.0-9a-zA-Z]+:|:|g" \
 )
 
-PATH="${TMP_PATH}"; export PATH
+export PATH="$(echo :"${TMP_PATH}": | sed -E 's|:+|:|g' | sed -E 's|^:+||' | sed -E 's|:+$||')"
 unset TMP_PATH
