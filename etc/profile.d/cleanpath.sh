263f856d1ec183a2c5c487f2c98f0de68a3fa2a8
MVA <mva000@gmail.com>
2024-11-10 20:50:40 +0900

    干渉するWindowsパスの削除方法を修正
     * 不要なコロン":"を削除

--- etc/profile.d/cleanpath.sh
+++ etc/profile.d/cleanpath.sh
@@ -1,12 +1,11 @@
 #
-TMP_PATH=$(echo :${PATH}: | sed \
+TMP_PATH=$(echo :"${PATH}": | sed \
   -e 's|:/c/[Ww][Ii][Nn][Ds][Oo][Ww][Ss]/System32/OpenSSH:|:|g' \
   -e 's|:/c/Strawberry/perl/bin:|:|g' \
   -e 's|:/c/Strawberry/perl/site/bin:|:|g' \
   -e 's|:/c/Strawberry/c/bin:|:|g' \
   -e 's|:/c/Program Files/Git/cmd:|:|g' \
-  -e 's|:+|:|g' -e 's|^:||' -e 's|:$||' \
 )
 
-PATH="${TMP_PATH}"; export PATH
+export PATH="$(echo :"${TMP_PATH}": | sed -E 's|:+|:|g' | sed -E 's|^:+||' | sed -E 's|:+$||')"
 unset TMP_PATH
